5c405130c20f040d931860e23eb6b80d2cd5f0ae
ryo-murai <murai.ry@gmail.com>
2016-03-23 20:29:54 +0900

    support grade and name

--- src/main/java/machisoccer/members/models/MemberPhoto.java
+++ src/main/java/machisoccer/members/models/MemberPhoto.java
@@ -12,17 +12,30 @@ public class MemberPhoto {
   @Getter
   private final String thumbnailUrl;
 
+  private final String grade;
+
+  private final String name;
+
   public MemberPhoto(String rawTitle, String photoUrl, String thumbnailUrl) {
     this.rawTitle = rawTitle;
     this.photoUrl = photoUrl;
     this.thumbnailUrl = thumbnailUrl;
+
+    String[] splits = rawTitle.split("・", 2);
+    if(splits.length>=2) {
+      grade = splits[0];
+      name = splits[1];
+    } else {
+      grade = rawTitle;
+      name = rawTitle;
+    }
   }
 
   public String getGrade() {
-    return rawTitle;
+    return grade;
   }
 
   public String getName() {
-    return rawTitle;
+    return name;
   }
 }
